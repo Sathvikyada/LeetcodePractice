29bacd790df1e49d627c42162cfa53deb37c3bcb
Sathvik Yadanaparthi <sathvik0423@gmail.com>
2024-03-28 11:00:20 -0400

    small change

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,10 +1,11 @@
 package Arrays;
+
+//EASY
+
 // Example 1:
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-import java.util.Arrays;
 
 // Example 2:
 // Input: nums = [3,2,4], target = 6
@@ -16,6 +17,7 @@ import java.util.Arrays;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Arrays;
 
 class Solution {
   public static int[] twoSum(int[] nums, int target) {
--- Puzzle/PalindromeNumber.java
+++ Puzzle/PalindromeNumber.java
@@ -1,5 +1,7 @@
 package Puzzle;
 
+//EASY
+
 // Example 1:
 // Input: x = 121
 // Output: true
